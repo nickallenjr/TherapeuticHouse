8a6d91f784798ccfdb5fa51a631e050fe0433958
Nicholas <nicholasallenjr@gmail.com>
2019-06-18 07:49:29 -0400

    rough draft responsive services page

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,3 +1,15 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("hamburger").style.visibility = "hidden"
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("hamburger").style.visibility = "visible"
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
     //do work	
     const nav = document.getElementById("navi");
@@ -9,10 +21,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
             TweenMax.to(nav, 1, { position: "fixed", backgroundColor: "#ffffff", top: 0, zIndex: 2 });
             TweenMax.to(".naviLinks", 1, { color: "#400F3C" });
             TweenMax.to(".naviTitle1", 1, { color: "#400F3C" });
+            TweenMax.to(".fas", 1, { color: "#400f3c" });
         } else {
             TweenMax.to(nav, 1, { position: "fixed", backgroundColor: "transparent" })
             TweenMax.to(".naviLinks", 1, { color: "#ffffff" });
-            TweenMax.to(".naviTitle1", 1, { color: "#ffffff" })
+            TweenMax.to(".naviTitle1", 1, { color: "#ffffff" });
+            TweenMax.to(".fas", 1, { color: "#ffffff" });
         }
     })
 
