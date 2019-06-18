b14f98e349ea1ac9a9c377448b57fc4959c42f67
Nicholas Allen <Nick@asherahs-imac.lan>
2019-06-18 11:39:34 -0400

    reponsive contact page

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,51 +1,63 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  //do work	
-	const nav = document.getElementById("navi");
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("hamburger").style.visibility = "hidden"
+}
 
-	window.addEventListener('scroll', function() {
-		const winTop = window.pageYOffset
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("hamburger").style.visibility = "visible"
+}
 
-		if (winTop > 0) {
-			TweenMax.to(nav, 1, {position:"fixed", backgroundColor:"#ffffff", top:0, zIndex:2});
-			TweenMax.to(".naviLinks", 1, {color:"#400F3C"});
-			TweenMax.to(".naviTitle1", 1, {color:"#400F3C"});
-		}else {
-			TweenMax.to(nav, 1, {position:"fixed", backgroundColor:"transparent"})
-			TweenMax.to(".naviLinks", 1, {color:"#ffffff"});
-			TweenMax.to(".naviTitle1", 1, {color:"#ffffff"})
-		}
-	})
+document.addEventListener("DOMContentLoaded", function(event) {
+    //do work	
+    const nav = document.getElementById("navi");
 
-	let mousedOver = function (navlink, eachlink) {
-		let link = document.getElementById(navlink);
+    window.addEventListener('scroll', function() {
+        const winTop = window.pageYOffset
 
-		link.onmouseenter = function() {
-			let split = new SplitText(eachlink, {type: "chars"});
-			const naviTl = new TimelineMax();
+        if (winTop > 0) {
+            TweenMax.to(nav, 1, { position: "fixed", backgroundColor: "#ffffff", top: 0, zIndex: 2 });
+            TweenMax.to(".naviLinks", 1, { color: "#400F3C" });
+            TweenMax.to(".naviTitle1", 1, { color: "#400F3C" });
+        } else {
+            TweenMax.to(nav, 1, { position: "fixed", backgroundColor: "transparent" })
+            TweenMax.to(".naviLinks", 1, { color: "#ffffff" });
+            TweenMax.to(".naviTitle1", 1, { color: "#ffffff" })
+        }
+    })
 
-			naviTl.staggerFromTo(split.chars, 2, {y:-20, opacity:0}, {opacity:1, y:0, ease:Power4.easeOut}, 0.1, "+=0.1")
-		}
-	}
+    let mousedOver = function(navlink, eachlink) {
+        let link = document.getElementById(navlink);
 
-	mousedOver("naviLinks1", "#homeLink");
-	mousedOver("naviLinks2", "#aboutLink");
-	mousedOver("naviLinks3", "#serviceLink");
-	mousedOver("naviLinks4", "#contactLink");
+        link.onmouseenter = function() {
+            let split = new SplitText(eachlink, { type: "chars" });
+            const naviTl = new TimelineMax();
+
+            naviTl.staggerFromTo(split.chars, 2, { y: -20, opacity: 0 }, { opacity: 1, y: 0, ease: Power4.easeOut }, 0.1, "+=0.1")
+        }
+    }
+
+    mousedOver("naviLinks1", "#homeLink");
+    mousedOver("naviLinks2", "#aboutLink");
+    mousedOver("naviLinks3", "#serviceLink");
+    mousedOver("naviLinks4", "#contactLink");
 });
 
 function initMap() {
-	const atlanta = {lat: 33.828600, lng: -84.366136};
-	const alpharetta = {lat:34.055269, lng: -84.231345};
+    const atlanta = { lat: 33.828600, lng: -84.366136 };
+    const alpharetta = { lat: 34.055269, lng: -84.231345 };
 
-	const map = new google.maps.Map(document.getElementById('map'), {
-		center: atlanta,
-		zoom: 12
-	});
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: atlanta,
+        zoom: 12
+    });
 
-	const marker = new google.maps.Marker({
-		position: atlanta, 
-		map: map,
-		animation: google.maps.Animation.DROP,
-	});
+    const marker = new google.maps.Marker({
+        position: atlanta,
+        map: map,
+        animation: google.maps.Animation.DROP,
+    });
 
 }
