5351f00e27e1a32377a8977b4976094def31d200
Nicholas <nicholasallenjr@gmail.com>
2019-06-18 20:22:05 -0400

    fixed form actions

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -21,10 +21,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
             TweenMax.to(nav, 1, { position: "fixed", backgroundColor: "#ffffff", top: 0, zIndex: 2 });
             TweenMax.to(".naviLinks", 1, { color: "#400F3C" });
             TweenMax.to(".naviTitle1", 1, { color: "#400F3C" });
+            TweenMax.to(".fas", 1, { color: "#400f3c" });
         } else {
             TweenMax.to(nav, 1, { position: "fixed", backgroundColor: "transparent" })
             TweenMax.to(".naviLinks", 1, { color: "#ffffff" });
             TweenMax.to(".naviTitle1", 1, { color: "#ffffff" })
+            TweenMax.to(".fas", 1, { color: "#ffffff" });
         }
     })
 
